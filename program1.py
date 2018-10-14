16adee3bcc2c1f46b4b79da6326405515263328f
Misznik <40390779+Misznik@users.noreply.github.com>
2018-10-14 14:55:50 +0200

    added MC simulation

--- program1.py
+++ program1.py
@@ -8,6 +8,7 @@ Created on Fri Oct 12 12:39:12 2018
 import pandas as pd
 import random as rd
 import math
+import matplotlib.pylab as plt
 
 dane_10_lat_df = pd.read_excel('team_v_team_10_makra.xlsm', sheetname='stosunki_simple')
 schedule_14_15_df = pd.read_excel('schedules.xlsx', sheetname='14-15')
@@ -16,306 +17,322 @@ dane_10_lat = dane_10_lat_df.values.tolist() #list of lists
 #dane_10_lat[ATL][1]
 schedule_14_15 = schedule_14_15_df.values.tolist()
 
-wyniki = [['ATL',0],
-['BOS',0],
-['NJN',0],
-['CHI',0],
-['CHA',0],
-['CLE',0],
-['DAL',0],
-['DEN',0],
-['DET',0],
-['GSW',0],
-['HOU',0],
-['IND',0],
-['LAC',0],
-['LAL',0],
-['MEM',0],
-['MIA',0],
-['MIL',0],
-['MIN',0],
-['NOH',0],
-['NYK',0],
-['SEA',0],
-['ORL',0],
-['PHI',0],
-['PHO',0],
-['POR',0],
-['SAC',0],
-['SAS',0],
-['TOR',0],
-['UTA',0],
-['WAS',0],
-]
+dict_champs = {'ATL':0,
+'BOS':0,
+'NJN':0,
+'CHI':0,
+'CHA':0,
+'CLE':0,
+'DAL':0,
+'DEN':0,
+'DET':0,
+'GSW':0,
+'HOU':0,
+'IND':0,
+'LAC':0,
+'LAL':0,
+'MEM':0,
+'MIA':0,
+'MIL':0,
+'MIN':0,
+'NOH':0,
+'NYK':0,
+'SEA':0,
+'ORL':0,
+'PHI':0,
+'PHO':0,
+'POR':0,
+'SAC':0,
+'SAS':0,
+'TOR':0,
+'UTA':0,
+'WAS':0}
 
 west_teams = ['DAL','DEN','GSW','HOU','LAC','MEM','MIN','NOH','SEA','PHO','POR','SAC','SAS','UTA']
 east_teams = ['BOS','NJN','CHI','CHA','CLE','DET','IND','MIA','MIL','NYK','ORL','PHI','TOR','WAS']
 
-#prob = dane_10_lat[0][1]/(dane_10_lat[0][1]+dane_10_lat[1][1])
-#print(rd.random() >= prob)
-
-#for i in range(1,len(dane_10_lat),1):
-    #print(dane_10_lat[i-1][0])
-    #print(wyniki[i-1][0])  #taka sama strunktura, moze slownik?
-    #print(dane_10_lat[i-1][0])
-ilosc_gier=0
-N=100
-
-#for n in range(0,N):
-for j in range(0,len(schedule_14_15[0])-1):  
-    for i in range(j+1,len(schedule_14_15[0])): #rozpoczecie petli od drugiego elementu, pomiciecie nazwy
-        #ilosc_gier+=schedule_14_15[j][i]
-        for number in range(0,schedule_14_15[j][i]):
-            prob = dane_10_lat[j][1]/(dane_10_lat[j][1]+dane_10_lat[i-1][1])
-            result = rd.random()
-            if result <= prob:
-                wyniki[j][1]+=1
-            else:
-                wyniki[i-1][1]+=1 #przez roznice w struktureze trzeba inaczej indeksowac
-
-#for n in range(0,len(wyniki)):
-#    wyniki[n][1]=round(wyniki[n][1]/N)
-#    ilosc_gier+=wyniki[n][1]
-
-#posortowane = wyniki
-#for n in range(0,len(posortowane)):
-#    posortowane[n].reverse() 
-#posortowane = sorted(posortowane, reverse = True)
-#print(wyniki)
-#print(ilosc_gier)
-
-east = []
-west = []
-
-for k in range(0,len(wyniki)):
-    if wyniki[k][0] in east_teams:
-        east.append(wyniki[k])
+N = 10000
+#############################  LOOPING
+for n in range(1,N):
+    wyniki = [['ATL',0],
+    ['BOS',0],
+    ['NJN',0],
+    ['CHI',0],
+    ['CHA',0],
+    ['CLE',0],
+    ['DAL',0],
+    ['DEN',0],
+    ['DET',0],
+    ['GSW',0],
+    ['HOU',0],
+    ['IND',0],
+    ['LAC',0],
+    ['LAL',0],
+    ['MEM',0],
+    ['MIA',0],
+    ['MIL',0],
+    ['MIN',0],
+    ['NOH',0],
+    ['NYK',0],
+    ['SEA',0],
+    ['ORL',0],
+    ['PHI',0],
+    ['PHO',0],
+    ['POR',0],
+    ['SAC',0],
+    ['SAS',0],
+    ['TOR',0],
+    ['UTA',0],
+    ['WAS',0]]
+    
+    for j in range(0,len(schedule_14_15[0])-1):  
+        for i in range(j+1,len(schedule_14_15[0])): #rozpoczecie petli od drugiego elementu, pomiciecie nazwy
+            for number in range(0,schedule_14_15[j][i]):
+                prob = dane_10_lat[j][1]/(dane_10_lat[j][1]+dane_10_lat[i-1][1])
+                result = rd.random()
+                if result <= prob:
+                    wyniki[j][1]+=1
+                else:
+                    wyniki[i-1][1]+=1 #przez roznice w struktureze trzeba inaczej indeksowac
+    
+    east = []
+    west = []
+    
+    for k in range(0,len(wyniki)):
+        if wyniki[k][0] in east_teams:
+            east.append(wyniki[k])
+        else:
+            west.append(wyniki[k])
+     
+    east.sort(key=lambda x: x[1], reverse = True) #sortowanie po drugim elemencie
+    west.sort(key=lambda x: x[1], reverse = True)
+            
+    rnd1_east = east[:8]
+    rnd1_west = west[:8]
+    ##################################   EAST 1st ROUND
+    rnd2_east = [[],[],[],[]]
+    win = [0,0,0,0,0,0,0,0]
+    while win[0] < 4 and win[7] < 4:  #NOWY POMYSL NA SYMULACJE PLAYOFFOW: WYNIKI Z TEGO SEZONU
+        prob = rnd1_east[0][1]/(rnd1_east[0][1]+rnd1_east[7][1])
+        result = rd.random()
+        if result <= prob:
+            win[0]+=1
+        else:
+            win[7]+=1
+    if win[0] == 4:
+        rnd2_east[0]=rnd1_east[0]
     else:
-        west.append(wyniki[k])
- 
-east.sort(key=lambda x: x[1], reverse = True) #sortowanie po drugim elemencie
-west.sort(key=lambda x: x[1], reverse = True)
+        rnd2_east[0]=rnd1_east[7]
         
-rnd1_east = east[:8]
-rnd1_west = west[:8]
-##################################   EAST 1st ROUND
-rnd2_east = [[],[],[],[]]
-win = [0,0,0,0,0,0,0,0]
-while win[0] < 4 and win[7] < 4:  #NOWY POMYSL NA SYMULACJE PLAYOFFOW: WYNIKI Z TEGO SEZONU
-    prob = rnd1_east[0][1]/(rnd1_east[0][1]+rnd1_east[7][1])
-    result = rd.random()
-    if result <= prob:
-        win[0]+=1
+    while win[3] < 4 and win[4] < 4:  #NOWY POMYSL NA SYMULACJE PLAYOFFOW: WYNIKI Z TEGO SEZONU
+        prob = rnd1_east[3][1]/(rnd1_east[3][1]+rnd1_east[4][1])
+        result = rd.random()
+        if result <= prob:
+            win[3]+=1
+        else:
+            win[4]+=1
+    if win[3] == 4:
+        rnd2_east[1]=rnd1_east[3]
     else:
-        win[7]+=1
-if win[0] == 4:
-    rnd2_east[0]=rnd1_east[0]
-else:
-    rnd2_east[0]=rnd1_east[7]
+        rnd2_east[1]=rnd1_east[4]
+        
+    while win[2] < 4 and win[5] < 4:  #NOWY POMYSL NA SYMULACJE PLAYOFFOW: WYNIKI Z TEGO SEZONU
+        prob = rnd1_east[2][1]/(rnd1_east[2][1]+rnd1_east[5][1])
+        result = rd.random()
+        if result <= prob:
+            win[2]+=1
+        else:
+            win[5]+=1
+    if win[2] == 4:
+        rnd2_east[2]=rnd1_east[2]
+    else:
+        rnd2_east[2]=rnd1_east[5]
+        
+    while win[1] < 4 and win[6] < 4:  #NOWY POMYSL NA SYMULACJE PLAYOFFOW: WYNIKI Z TEGO SEZONU
+        prob = rnd1_east[1][1]/(rnd1_east[1][1]+rnd1_east[6][1])
+        result = rd.random()
+        if result <= prob:
+            win[1]+=1
+        else:
+            win[6]+=1
+    if win[1] == 4:
+        rnd2_east[3]=rnd1_east[1]
+    else:
+        rnd2_east[3]=rnd1_east[6]
+        
+    ##########################################################   WEST 1st ROUND
+    rnd2_west = [[],[],[],[]]
+    win = [0,0,0,0,0,0,0,0]
+    while win[0] < 4 and win[7] < 4:  #NOWY POMYSL NA SYMULACJE PLAYOFFOW: WYNIKI Z TEGO SEZONU
+        prob = rnd1_west[0][1]/(rnd1_west[0][1]+rnd1_west[7][1])
+        result = rd.random()
+        if result <= prob:
+            win[0]+=1
+        else:
+            win[7]+=1
+    if win[0] == 4:
+        rnd2_west[0]=rnd1_west[0]
+    else:
+        rnd2_west[0]=rnd1_west[7]
+        
+    while win[3] < 4 and win[4] < 4:  #NOWY POMYSL NA SYMULACJE PLAYOFFOW: WYNIKI Z TEGO SEZONU
+        prob = rnd1_west[3][1]/(rnd1_west[3][1]+rnd1_west[4][1])
+        result = rd.random()
+        if result <= prob:
+            win[3]+=1
+        else:
+            win[4]+=1
+    if win[3] == 4:
+        rnd2_west[1]=rnd1_west[3]
+    else:
+        rnd2_west[1]=rnd1_west[4]
+        
+    while win[2] < 4 and win[5] < 4:  #NOWY POMYSL NA SYMULACJE PLAYOFFOW: WYNIKI Z TEGO SEZONU
+        prob = rnd1_west[2][1]/(rnd1_west[2][1]+rnd1_west[5][1])
+        result = rd.random()
+        if result <= prob:
+            win[2]+=1
+        else:
+            win[5]+=1
+    if win[2] == 4:
+        rnd2_west[2]=rnd1_west[2]
+    else:
+        rnd2_west[2]=rnd1_west[5]
+        
+    while win[1] < 4 and win[6] < 4:  #NOWY POMYSL NA SYMULACJE PLAYOFFOW: WYNIKI Z TEGO SEZONU
+        prob = rnd1_west[1][1]/(rnd1_west[1][1]+rnd1_west[6][1])
+        result = rd.random()
+        if result <= prob:
+            win[1]+=1
+        else:
+            win[6]+=1
+    if win[1] == 4:
+        rnd2_west[3]=rnd1_west[1]
+    else:
+        rnd2_west[3]=rnd1_west[6]
+    #############################################################   RND2 EAST     
+    rnd3_east = [[],[]]
+    win = [0,0,0,0]
+    while win[0] < 4 and win[1] < 4: 
+        prob = rnd2_east[0][1]/(rnd2_east[0][1]+rnd2_east[1][1])
+        result = rd.random()
+        if result <= prob:
+            win[0]+=1
+        else:
+            win[1]+=1
+    if win[0] == 4:
+        rnd3_east[0]=rnd2_east[0]
+    else:
+        rnd3_east[0]=rnd2_east[1]    
+     
+    while win[2] < 4 and win[3] < 4: 
+        prob = rnd2_east[2][1]/(rnd2_east[2][1]+rnd2_east[3][1])
+        result = rd.random()
+        if result <= prob:
+            win[2]+=1
+        else:
+            win[3]+=1
+    if win[2] == 4:
+        rnd3_east[1]=rnd2_east[2]
+    else:
+        rnd3_east[1]=rnd2_east[3]  
     
-while win[3] < 4 and win[4] < 4:  #NOWY POMYSL NA SYMULACJE PLAYOFFOW: WYNIKI Z TEGO SEZONU
-    prob = rnd1_east[3][1]/(rnd1_east[3][1]+rnd1_east[4][1])
-    result = rd.random()
-    if result <= prob:
-        win[3]+=1
+    #############################################################   RND2 WEST     
+    rnd3_west = [[],[]]
+    win = [0,0,0,0]
+    while win[0] < 4 and win[1] < 4: 
+        prob = rnd2_west[0][1]/(rnd2_west[0][1]+rnd2_west[1][1])
+        result = rd.random()
+        if result <= prob:
+            win[0]+=1
+        else:
+            win[1]+=1
+    if win[0] == 4:
+        rnd3_west[0]=rnd2_west[0]
     else:
-        win[4]+=1
-if win[3] == 4:
-    rnd2_east[1]=rnd1_east[3]
-else:
-    rnd2_east[1]=rnd1_east[4]
-    
-while win[2] < 4 and win[5] < 4:  #NOWY POMYSL NA SYMULACJE PLAYOFFOW: WYNIKI Z TEGO SEZONU
-    prob = rnd1_east[2][1]/(rnd1_east[2][1]+rnd1_east[5][1])
-    result = rd.random()
-    if result <= prob:
-        win[2]+=1
+        rnd3_west[0]=rnd2_west[1]    
+     
+    while win[2] < 4 and win[3] < 4: 
+        prob = rnd2_west[2][1]/(rnd2_west[2][1]+rnd2_west[3][1])
+        result = rd.random()
+        if result <= prob:
+            win[2]+=1
+        else:
+            win[3]+=1
+    if win[2] == 4:
+        rnd3_west[1]=rnd2_west[2]
     else:
-        win[5]+=1
-if win[2] == 4:
-    rnd2_east[2]=rnd1_east[2]
-else:
-    rnd2_east[2]=rnd1_east[5]
-    
-while win[1] < 4 and win[6] < 4:  #NOWY POMYSL NA SYMULACJE PLAYOFFOW: WYNIKI Z TEGO SEZONU
-    prob = rnd1_east[1][1]/(rnd1_east[1][1]+rnd1_east[6][1])
-    result = rd.random()
-    if result <= prob:
-        win[1]+=1
+        rnd3_west[1]=rnd2_west[3]  
+    ########################################  CONFERENCE FINALS
+    final=[[],[]]
+    win = [0,0]
+    while win[0] < 4 and win[1] < 4: 
+        prob = rnd3_east[0][1]/(rnd3_east[0][1]+rnd3_east[1][1])
+        result = rd.random()
+        if result <= prob:
+            win[0]+=1
+        else:
+            win[1]+=1
+    if win[0] == 4:
+        final[0]=rnd3_east[0]
     else:
-        win[6]+=1
-if win[1] == 4:
-    rnd2_east[3]=rnd1_east[1]
-else:
-    rnd2_east[3]=rnd1_east[6]
-    
-##########################################################   WEST 1st ROUND
-rnd2_west = [[],[],[],[]]
-win = [0,0,0,0,0,0,0,0]
-while win[0] < 4 and win[7] < 4:  #NOWY POMYSL NA SYMULACJE PLAYOFFOW: WYNIKI Z TEGO SEZONU
-    prob = rnd1_west[0][1]/(rnd1_west[0][1]+rnd1_west[7][1])
-    result = rd.random()
-    if result <= prob:
-        win[0]+=1
+        final[0]=rnd3_east[1]
+        
+    win = [0,0]
+    while win[0] < 4 and win[1] < 4: 
+        prob = rnd3_west[0][1]/(rnd3_west[0][1]+rnd3_west[1][1])
+        result = rd.random()
+        if result <= prob:
+            win[0]+=1
+        else:
+            win[1]+=1
+    if win[0] == 4:
+        final[1]=rnd3_west[0]
     else:
-        win[7]+=1
-if win[0] == 4:
-    rnd2_west[0]=rnd1_west[0]
-else:
-    rnd2_west[0]=rnd1_west[7]
-    
-while win[3] < 4 and win[4] < 4:  #NOWY POMYSL NA SYMULACJE PLAYOFFOW: WYNIKI Z TEGO SEZONU
-    prob = rnd1_west[3][1]/(rnd1_west[3][1]+rnd1_west[4][1])
-    result = rd.random()
-    if result <= prob:
-        win[3]+=1
+        final[1]=rnd3_west[1]   
+        
+    ########################################  FINALS
+    champion=[[]]
+    win = [0,0]
+    while win[0] < 4 and win[1] < 4: 
+        prob = final[0][1]/(final[0][1]+final[1][1])
+        result = rd.random()
+        if result <= prob:
+            win[0]+=1
+        else:
+            win[1]+=1
+    if win[0] == 4:
+        champion[0]=final[0]
     else:
-        win[4]+=1
-if win[3] == 4:
-    rnd2_west[1]=rnd1_west[3]
-else:
-    rnd2_west[1]=rnd1_west[4]
-    
-while win[2] < 4 and win[5] < 4:  #NOWY POMYSL NA SYMULACJE PLAYOFFOW: WYNIKI Z TEGO SEZONU
-    prob = rnd1_west[2][1]/(rnd1_west[2][1]+rnd1_west[5][1])
-    result = rd.random()
-    if result <= prob:
-        win[2]+=1
-    else:
-        win[5]+=1
-if win[2] == 4:
-    rnd2_west[2]=rnd1_west[2]
-else:
-    rnd2_west[2]=rnd1_west[5]
-    
-while win[1] < 4 and win[6] < 4:  #NOWY POMYSL NA SYMULACJE PLAYOFFOW: WYNIKI Z TEGO SEZONU
-    prob = rnd1_west[1][1]/(rnd1_west[1][1]+rnd1_west[6][1])
-    result = rd.random()
-    if result <= prob:
-        win[1]+=1
-    else:
-        win[6]+=1
-if win[1] == 4:
-    rnd2_west[3]=rnd1_west[1]
-else:
-    rnd2_west[3]=rnd1_west[6]
-#############################################################   RND2 EAST     
-rnd3_east = [[],[]]
-win = [0,0,0,0]
-while win[0] < 4 and win[1] < 4: 
-    prob = rnd2_east[0][1]/(rnd2_east[0][1]+rnd2_east[1][1])
-    result = rd.random()
-    if result <= prob:
-        win[0]+=1
-    else:
-        win[1]+=1
-if win[0] == 4:
-    rnd3_east[0]=rnd2_east[0]
-else:
-    rnd3_east[0]=rnd2_east[1]    
- 
-while win[2] < 4 and win[3] < 4: 
-    prob = rnd2_east[2][1]/(rnd2_east[2][1]+rnd2_east[3][1])
-    result = rd.random()
-    if result <= prob:
-        win[2]+=1
-    else:
-        win[3]+=1
-if win[2] == 4:
-    rnd3_east[1]=rnd2_east[2]
-else:
-    rnd3_east[1]=rnd2_east[3]  
+        champion[0]=final[1]
+        
+    dict_champs[champion[0][0]]+=1
 
-#############################################################   RND2 WEST     
-rnd3_west = [[],[]]
-win = [0,0,0,0]
-while win[0] < 4 and win[1] < 4: 
-    prob = rnd2_west[0][1]/(rnd2_west[0][1]+rnd2_west[1][1])
-    result = rd.random()
-    if result <= prob:
-        win[0]+=1
-    else:
-        win[1]+=1
-if win[0] == 4:
-    rnd3_west[0]=rnd2_west[0]
-else:
-    rnd3_west[0]=rnd2_west[1]    
- 
-while win[2] < 4 and win[3] < 4: 
-    prob = rnd2_west[2][1]/(rnd2_west[2][1]+rnd2_west[3][1])
-    result = rd.random()
-    if result <= prob:
-        win[2]+=1
-    else:
-        win[3]+=1
-if win[2] == 4:
-    rnd3_west[1]=rnd2_west[2]
-else:
-    rnd3_west[1]=rnd2_west[3]  
-########################################  CONFERENCE FINALS
-final=[[],[]]
-win = [0,0]
-while win[0] < 4 and win[1] < 4: 
-    prob = rnd3_east[0][1]/(rnd3_east[0][1]+rnd3_east[1][1])
-    result = rd.random()
-    if result <= prob:
-        win[0]+=1
-    else:
-        win[1]+=1
-if win[0] == 4:
-    final[0]=rnd3_east[0]
-else:
-    final[0]=rnd3_east[1]
-    
-win = [0,0]
-while win[0] < 4 and win[1] < 4: 
-    prob = rnd3_west[0][1]/(rnd3_west[0][1]+rnd3_west[1][1])
-    result = rd.random()
-    if result <= prob:
-        win[0]+=1
-    else:
-        win[1]+=1
-if win[0] == 4:
-    final[1]=rnd3_west[0]
-else:
-    final[1]=rnd3_west[1]   
-    
-########################################  FINALS
-champion=[[]]
-win = [0,0]
-while win[0] < 4 and win[1] < 4: 
-    prob = final[0][1]/(final[0][1]+final[1][1])
-    result = rd.random()
-    if result <= prob:
-        win[0]+=1
-    else:
-        win[1]+=1
-if win[0] == 4:
-    champion[0]=final[0]
-else:
-    champion[0]=final[1]
+names = list(dict_champs.keys())
+values = list(dict_champs.values())
 
-print('Eastern conference:')
-print(east)
-print('Western conference:')
-print(west)
-
-print('PLAYOFFS 1st Round:')   #SCHEMAT
-print('East:')
-print(rnd1_east)
-print('West:')
-print(rnd1_west)
-print('PLAYOFFS 2nd Round:')   
-print('East:')
-print(rnd2_east)
-print('West:')
-print(rnd2_west)
-print('Conference finals:')   
-print('East:')
-print(rnd3_east)
-print('West:')
-print(rnd3_west)
-print('Finals:')
-print(final)
-print('Champion:')
-print(champion)
+plt.bar(range(len(dict_champs)),values,tick_label=names)
+plt.show()
+#print('Eastern conference:')
+#print(east)
+#print('Western conference:')
+#print(west)
+#print('PLAYOFFS 1st Round:')   
+#print('East:')
+#print(rnd1_east)
+#print('West:')
+#print(rnd1_west)
+#print('PLAYOFFS 2nd Round:')   
+#print('East:')
+#print(rnd2_east)
+#print('West:')
+#print(rnd2_west)
+#print('Conference finals:')   
+#print('East:')
+#print(rnd3_east)
+#print('West:')
+#print(rnd3_west)
+#print('Finals:')
+#print(final)
+#print('Champion:')
+#print(champion)
